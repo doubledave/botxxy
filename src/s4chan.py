9dd791676ac17251c129a0e121bc1b8cf2644654
b0nk <fsantos.pt@hotmail.com>
2013-10-22 19:58:57 +0100

    4chan quotes (greentext) are now green in IRC

--- src/s4chan.py
+++ src/s4chan.py
@@ -13,12 +13,12 @@ def s4Chan(board, search):
         subject = j.get('sub', 'Empty subject')
         post = p.unescape(j.get('com', 'Empty post'))
         post = post.replace('<br>', ' ')
-        post = post.replace('<span class="quote">','')
-        post = post.replace('</span>','')
+        post = post.replace('<span class="quote">','3') #greentext open
+        post = post.replace('</span>','') #close color
 
         if len(post) > 300:
           post = post[0:300]
-          post = post + '...'
+          post = post + '...' #close color here also
 
         text = '%s /%s/ %s | %s | %s (R:%s, I:%s)' % (logo, board, subject, post, 'https://4chan.org/%s/res/%s' % (board, j['no']), j['replies'], j['images'])
         res.append(text)
