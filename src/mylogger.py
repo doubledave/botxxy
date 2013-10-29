5cecbb77064d9c8eabe4cce10735f6bd3bab0458
b0nk <fsantos.pt@hotmail.com>
2013-10-29 19:05:54 +0000

    append behaviour

--- src/mylogger.py
+++ src/mylogger.py
@@ -1,12 +1,11 @@
 import sys, time
 
 class Logger(object):
-    def __init__(self):
-        self.terminal = sys.stdout
-        self.log = open("botlog.log", "w", 0)
+  def __init__(self):
+    self.terminal = sys.stdout
+    self.log = open("botlog.log", "a", 0)
 
-    def write(self, message):
-        self.terminal.write(message)
-        t = time.strftime('%H:%M:%S', time.localtime())
-        self.log.write("[%s] %s" % (t, message))
-        
+  def write(self, message):
+    self.terminal.write(message)
+    t = time.strftime('%H:%M:%S', time.localtime())
+    self.log.write("[%s] %s" % (t, message))
